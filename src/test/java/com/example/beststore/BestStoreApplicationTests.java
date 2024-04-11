c82afe36fbe4edec83a847a705cd4871868effc4
Swapnil <swapnilamode9175@gmail.com>
2024-04-11 15:03:42 +0530

    First commit

---
+++ src/test/java/com/example/beststore/BestStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beststore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
